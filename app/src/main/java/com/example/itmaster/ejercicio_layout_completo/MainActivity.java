dd0c7f80e3b57d6941d20db60e9b1347d78e4529
modicristian <cristianmodi@gmail.com>
2018-07-12 21:59:46 -0300

    Realizamos findviewByid

--- app/src/main/java/com/example/itmaster/ejercicio_layout_completo/MainActivity.java
+++ app/src/main/java/com/example/itmaster/ejercicio_layout_completo/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.itmaster.ejercicio_layout_completo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button peliculas1,peliculas2,peliculas3,peliculas4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peliculas1 = findViewById(R.id.peliculas1);
+        peliculas2 = findViewById(R.id.peliculas2);
+        peliculas3 = findViewById(R.id.peliculas3);
+        peliculas4 = findViewById(R.id.peliculas4);
+
+        peliculas1.setText("VOLVER AL FUTURO");
+        peliculas2.setText("VOLVER AL FUTURO 2");
+        peliculas3.setText("VOLVER AL FUTURO 3");
+        peliculas4.setText("ALIEN");
     }
 }
